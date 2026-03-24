3e5868f7bce1618dac2fe00ac23c318a2aeff23d
bienv-007 <bienvvasima@gmail.com>
2026-03-24 16:01:06 +0200

    fin

--- connect_db.php
+++ connect_db.php
@@ -1,7 +1,7 @@
 <?php
 try{
     $connexion = new PDO("mysql:host=localhost;dbname=minichat", "root", "");
-$connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     echo "Erreur de connexion : " . $e->getMessage();
 }
